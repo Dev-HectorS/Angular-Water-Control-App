26a58229f63c08a183d54e86b755c72c5fce4aef
Dev-HectorS <hector.shm981@gmail.com>
2021-04-06 18:46:20 -0500

    Create: WaterModule

---
+++ src/app/water/water.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WaterRoutingModule } from './water-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WaterRoutingModule
+  ]
+})
+export class WaterModule { }
