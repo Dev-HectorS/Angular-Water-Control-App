e926e3bc6ff3e289d5d19044ecad461b8e7fa3bb
Dev-HectorS <hector.shm981@gmail.com>
2021-04-06 22:29:12 -0500

    Update: Import WaterModule using LazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./water/water.module').then(m => m.WaterModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
