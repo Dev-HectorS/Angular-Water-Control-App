2b4198fc5642ff5602d38d86177aeef30bf18bcb
Dev-HectorS <hector.shm981@gmail.com>
2021-04-06 21:41:57 -0500

    Create: Main pages

--- src/app/water/water.module.ts
+++ src/app/water/water.module.ts
@@ -3,9 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { WaterRoutingModule } from './water-routing.module';
 
+import { HomeComponent } from './pages/home/home.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { UsersComponent } from './pages/users/users.component';
+import { UserComponent } from './pages/user/user.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AddComponent } from './pages/add/add.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    NotificationsComponent,
+    UsersComponent,
+    UserComponent,
+    SettingsComponent,
+    AddComponent
+  ],
   imports: [
     CommonModule,
     WaterRoutingModule
