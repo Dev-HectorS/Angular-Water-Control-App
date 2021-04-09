fb66231450cf72f85dc9621e52a404720d28d8b9
Dev-HectorS <hector.shm981@gmail.com>
2021-04-09 01:03:17 -0500

    Update: firebase variable set

--- src/app/water/services/users.service.ts
+++ src/app/water/services/users.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+
+  private apiUrl: string = environment.firebase
 
   constructor() { }
 }
