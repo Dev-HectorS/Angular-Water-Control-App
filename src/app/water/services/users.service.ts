52dc6f615818db127270b57a704bcb91a6db6e10
Dev-HectorS <hector.shm981@gmail.com>
2021-04-12 19:35:12 -0500

    Create: get users/ users.service

--- src/app/water/services/users.service.ts
+++ src/app/water/services/users.service.ts
@@ -13,10 +13,29 @@ export class UsersService {
 
   private apiUrl: string = environment.firebase
 
+  private createUsersArry(usersObj: object) {
+    const users: User[] = [];
+    if (usersObj === null) {
+      return [];
+    }
+    Object.keys(usersObj).forEach((key) => {
+      const user: User = (usersObj as any)[key];
+      user.id = key;
+      users.push(user);
+    });
+    return users;
+  }
+
   constructor(private http: HttpClient) { }
 
   createUser(user: User): Observable<User> {
     return this.http.post<User>(`${this.apiUrl}/users.json`, user);
   }
 
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users.json`)
+      .pipe(
+        map((resp) => this.createUsersArry(resp))
+      );
+  }
 }
