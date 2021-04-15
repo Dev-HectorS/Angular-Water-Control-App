45b97b3b04d61c3db195e7ce91bc8ec28b8645d0
Dev-HectorS <hector.shm981@gmail.com>
2021-04-14 19:00:32 -0500

    Create: get user/ users.service

--- src/app/water/pages/user/user.component.ts
+++ src/app/water/pages/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { UsersService } from '../../services/users.service';
+import { Month, User, Year } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+  userYears!: Year[];
+  years: string[] = [];
+  months: Month[] = [];
+  id: string = '';
+
+  form: FormGroup = this._formBuilder.group({
+    year: ['', Validators.required]
+  })
+
+  constructor(private _formBuilder: FormBuilder,
+    private usersService: UsersService,
+    private routes: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.routes.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.usersService.getUserId(id)
+        .subscribe((resp: any) => {
+          this.user = resp;
+          this.user.id = id;
+          this.usersService.getUserYears(this.user.id)
+            .subscribe((resp) => {
+              this.userYears = resp
+              this.month(resp);
+            });
+        })
+      this.form.get('year')?.valueChanges
+        .subscribe((i: number) =>
+          this.usersService.getMonths(this.userYears[i], id)
+            .subscribe((resp) => {
+              this.months = resp;
+            })
+        );
+    }
   }
 
+  month(resp: any) {
+    for (let i = 0; i < resp.length; i++) {
+      this.years.push(resp[i].year)
+    }
+  }
 }
